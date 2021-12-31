b30ab6737774f5b447d1ddfd7d88124154a755b6
Ann A <31941055+an-rainbow@users.noreply.github.com>
2021-12-31 01:57:04 -0800

    Create webconfig.java

---
+++ src/main/configure/webconfig.java
@@ -0,0 +1,20 @@
+package com.df.config;
+
+import com.df.interceptor.RedisUrlCountInterceptor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+
+@Configuration
+public class WebConfigurer implements WebMvcConfigurer {
+
+    @Autowired
+    private RedisUrlCountInterceptor redisUrlCountInterceptor;
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(redisUrlCountInterceptor).addPathPatterns("/**");
+    }
+}
