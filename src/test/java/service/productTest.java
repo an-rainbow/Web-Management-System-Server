3444fb8d60559fc3599d06df2f38353b62c0b221
Ann A <31941055+an-rainbow@users.noreply.github.com>
2021-12-31 02:01:32 -0800

    Create productTest.java

---
+++ src/test/java/service/productTest.java
@@ -0,0 +1,30 @@
+package com.df.service;
+
+import com.df.pojo.Products;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.UUID;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+
+@SpringBootTest
+@ActiveProfiles("test")
+@Transactional
+class ProductsServiceTest {
+
+    @Autowired
+    private ProductsService productsService;
+
+    @Test
+    void updateProductById() {
+        int id = productsService.updateProductById(new Products(2, "1.jpg", 1,
+                UUID.randomUUID().toString().replace("-", ""), "小米9",
+                "手机", "2390", "5", "0", "", 0));
+        assertThat(id).isEqualTo(1);
+    }
+}
