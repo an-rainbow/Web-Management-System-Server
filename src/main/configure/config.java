6493b849de33bd73524e497739a8baccfd4411dd
Ann A <31941055+an-rainbow@users.noreply.github.com>
2021-12-31 01:54:40 -0800

    Create config.java

---
+++ src/main/configure/config.java
@@ -0,0 +1,28 @@
+package com.df.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+
+public class GlobalCorsConfig {
+
+    
+    public WebMvcConfigurer corsConfigurer(){
+
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins("*")
+                        .allowCredentials(true)
+                        .allowedMethods("GET","POST","PUT","DELETE","PATCH")
+                        .allowedHeaders("*")
+                        .exposedHeaders("Header1","Header2");
+            }
+        };
+    }
+
+
+}
